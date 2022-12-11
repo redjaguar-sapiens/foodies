5fc22435cfc6dda64cb81929272d335da79f32ae
RedJaguar <tristanojbacon+redjaguar@gmail.com>
2022-12-11 19:57:16 +0000

    Fixed fired/unfired localization

--- scripts/common/resource.lua
+++ scripts/common/resource.lua
@@ -11,8 +11,8 @@ function mod:onload(resource)
 
     typeMaps:insert("resource", resource.types, {
         key = "unfiredBowlGruel",
-        name = locale:get("resource_gruel"),
-        plural = locale:get("resource_gruel_plural"),
+        name = locale:get("resource_unfiredBowlGruel"),
+        plural = locale:get("resource_unfiredBowlGruel_plural"),
         foodValue = 1,
         foodPortionCount = 1,
         displayGameObjectTypeIndex = gameObjectTypeIndexMap.unfiredBowlGruel,
@@ -20,8 +20,8 @@ function mod:onload(resource)
 
     typeMaps:insert("resource", resource.types, {
         key = "firedBowlGruel",
-        name = locale:get("resource_gruel"),
-        plural = locale:get("resource_gruel_plural"),
+        name = locale:get("resource_firedBowlGruel"),
+        plural = locale:get("resource_firedBowlGruel_plural"),
         foodValue = 1,
         foodPortionCount = 1,
         displayGameObjectTypeIndex = gameObjectTypeIndexMap.firedBowlGruel,
